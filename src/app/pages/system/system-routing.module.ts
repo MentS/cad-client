176d4d4ea5968d9167a2178e34d68bef78e7bd60
Leon <997765875@qq.com>
2019-11-13 17:36:06 +0800

    supply

--- src/app/pages/system/system-routing.module.ts
+++ src/app/pages/system/system-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CustComponent } from './cust/cust.component';
+import { SupplyComponent } from './supply/supply.component';
 
-const routes: Routes = [{ path: 'cust', component: CustComponent }];
+const routes: Routes = [
+  { path: 'cust', component: CustComponent },
+  { path: 'supply', component: SupplyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/system/system.module.ts
+++ src/app/pages/system/system.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 
+import { SharedModule } from '../../shared/shared.module';
 import { SystemRoutingModule } from './system-routing.module';
 import { CustComponent } from './cust/cust.component';
+import { SupplyComponent } from './supply/supply.component';
 
 @NgModule({
-  declarations: [CustComponent],
-  imports: [SystemRoutingModule, NgZorroAntdModule]
+  declarations: [CustComponent, SupplyComponent],
+  imports: [SharedModule, SystemRoutingModule, NgZorroAntdModule]
 })
 export class SystemModule {}
